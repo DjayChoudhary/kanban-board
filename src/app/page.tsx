f41a5fbbb23c7a5af51836229a0af877297187c0
Dhananjay Choudhary <dhananjay2536@gmail.com>
2023-11-02 01:51:18 +0530

    remove Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Board from "@/components/Board";
-import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="h-screen flex flex-col">
-      <Header />
+      <header className="font-mono px-4 pt-4 pb-3 flex gap-12 items-center w-full border-b-2 border-gray-300">
+        <span className="text-2xl font-medium">Kanban Board</span>
+        <button className="bg-blue-600 hover:bg-opacity-95 rounded-md text-slate-50 px-4 py-2">
+          + Create Task
+        </button>
+      </header>
       <section className="m-8 flex-auto relative flex">
         <Board />
       </section>
