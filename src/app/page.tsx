cffe4808a325bac7ab2023ff4b2066c1f05a95de
Dhananjay Choudhary <dhananjay2536@gmail.com>
2023-11-02 01:48:41 +0530

    add Board component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-screen flex flex-col">
       <Header />
+      <section className="m-8 flex-auto relative flex">
+        <Board />
+      </section>
     </main>
   );
 }
