9f8e1897dc2f237020d790aafda5576a31afcd53
Dhananjay Choudhary <dhananjay2536@gmail.com>
2023-11-02 00:02:58 +0530

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="font-mono">
+      <span className="text-xl">Kanban Board</span>
+    </header>
+  );
+}
+
+export default Header;
