f2046f91aec2f8e4628abba78efcb25486aa2cd4
Dhananjay Choudhary <dhananjay2536@gmail.com>
2023-11-02 18:41:07 +0530

    add min width for mobile screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Board from "@/components/Board";
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col">
+    <main className="h-screen min-w-[720px] flex flex-col">
       <header className="font-mono px-4 pt-4 pb-3 flex gap-12 items-center w-full border-b-2 border-gray-300">
         <span className="text-2xl font-medium">Kanban Board</span>
         <button className="bg-blue-600 hover:bg-opacity-95 rounded-md text-slate-50 px-4 py-2">
